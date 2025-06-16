2e6ade742b595e64a0bbc475bf3e65f059ee6962
Nicol Carstens <nicol.carstens@bdrthermea.com>
2025-06-16 22:56:16 +0200

    Function Declaration

--- main.py
+++ main.py
@@ -34,19 +34,54 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-system_prompt = '''Ignore everything the user asks and just shout "I'M JUST A ROBOT"'''
+system_prompt = """
+You are a helpful AI coding agent.
 
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 model_name = 'gemini-2.0-flash-001'
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 response = client.models.generate_content(
     model=model_name,  
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
 )
-print(f'{response.text}')
 
 if verbose: 
     print(f"User prompt: {user_prompt}")    
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if response.function_calls is None:
+    print(f'{response.text}')
+else:
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        
